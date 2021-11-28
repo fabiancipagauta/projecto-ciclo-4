b96e4d6d4e2c3470180299b0eb577be2732bfbce
fabiancipagauta <fcipagauta@uninorte.edu.co>
2021-11-27 21:13:12 -0500

    quitar mensaje prueba

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -4,7 +4,7 @@ import { Login } from './Login';
 export const LoginPage = () => {
   return (
     <div className="container align-items-center mt-5">
-      <h2 className="text-center text-primary">Plataforma Sistema de Nomina Prueba</h2>
+      <h2 className="text-center text-primary">Plataforma Sistema de Nomina</h2>
       <Login />
     </div>
   );
